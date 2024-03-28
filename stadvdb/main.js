bd9f7586e567df087ea2b27b80b273dcc9a33369
Samoyedeu <samcaasi30@gmail.com>
2024-03-29 01:45:55 +0800

    Frontend changes and reports draft

--- stadvdb/main.js
+++ stadvdb/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const insertForm = document.querySelector("#insertForm");
-    const searchForm = document.querySelector("#searchForm"); // Add this line
+    const searchForm = document.querySelector("#searchForm"); 
+    const updateForm = document.querySelector("#updateForm");
+    const totalCountAppointments = document.querySelector("#totalAppointments"); 
 
     insertForm.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -30,6 +32,38 @@ document.addEventListener("DOMContentLoaded", function() {
             });
     });
 
+    updateForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(document.querySelector("#updateForm"));
+        const jsonData = convertFormDataToJson(formData);
+        postData('/updateAppt', jsonData)
+            .then(data => {
+                console.log(data);
+                alert('Appointment updated successfully!');
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    });
+
+    // Text-based reports functions
+    // Function to generate the total count of appointments report
+    totalCountAppointments.addEventListener('click', generateTotalCountAppointmentsReport);
+    function generateTotalCountAppointmentsReport() {
+        const selectedRegion = document.querySelector("#totalAppointments").value;
+        const url = '/totalCountAppointments' + (selectedRegion !== 'all' ? `?region=${selectedRegion}` : '');
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // Display the data in the `#reportsResults` div
+                document.querySelector("#reportsResults").innerHTML = `<p>Total Count of Appointments: ${data.totalAppointments}</p>`;
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
+
     function convertFormDataToJson(formData) {
         const jsonData = {};
         formData.forEach((value, key) => {
@@ -53,4 +87,6 @@ document.addEventListener("DOMContentLoaded", function() {
             throw new Error('Network response was not ok.');
         });
     }
+
+
 });
